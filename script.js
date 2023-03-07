0879f6669a2e3c7caf994d1214442758588c0a59
Arthur Oliveira <arthurd3v@gmail.com>
2023-03-07 19:28:54 -0300

    adicionado animação ao scroll e carregamento suave

--- script.js
+++ script.js
@@ -13,3 +13,34 @@ habLi.forEach((itemMenu, index) => {
     activeTab(index);
   });
 });
+
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute('href');
+  const section = document.querySelector(href);
+
+  section.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
+}
+
+linksInternos.forEach((link) => {
+  link.addEventListener('click', scrollToSection);
+});
+
+const sections = document.querySelectorAll('.js-scroll');
+const Metade = window.innerHeight * 0.6;
+
+function animaScroll() {
+  sections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top - Metade;
+    if (sectionTop < 0) {
+      section.classList.add('ativo');
+    }
+  });
+}
+
+window.addEventListener('scroll', animaScroll);
